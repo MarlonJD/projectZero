08fecd8c1926b88e4a55b07c50a7eba06efdecec
Burak Karahan <burak.karahan@mail.ru>
2019-12-30 22:44:15 +0300

    Add models

--- projectZero/panel/models.py
+++ projectZero/panel/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.conf import settings
+
+'''
+-----------------------------
+    Distribution Models
+-----------------------------
+Artist, OtherArtist, Partner,
+RecordLabel, Track, Album
+-----------------------------
+'''
 
 
 # Artist is Artist(name=String)
 # interp. Main Artist of the media
+#         user is User as artist's usership
 #         name is the main artist name of media       String
+#         appleArtist, spotifyArtist is the URL
 class Artist(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    appleArtist = models.URLField(null=True)
+    spotifyArtist = models.URLField(null=True)
 
     def __str__(self):
         return self.name
@@ -24,23 +40,38 @@ class OtherArtist(models.Model):
         return self.artist.name + ' ' + str(self.rate) + '%'
 
 
-# Partner is Partner(name=String)
-# interp. Partner of the that media will share in
+# Platform is Platform(name=String)
+# interp. Platform of the that media will share in
 #         id is the primary key that automaticly
 #            given and use as enumatation
 #         name is the partner's name                 String
-class Partner(models.Model):
+class Platform(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
+# RecordLabel is RecordLabel(user=User, name=String)
+# interp. RecordLabel is the label company for media
+#         user is the label company user (if any)
+#         name is the label name
+class RecordLabel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             null=True)
+    name = models.CharField(max_length=100)
+
+
 # Track is Track(media=FILE)
 # interp. track is the media file
 #         media is the FILE
+#         otherArtists is OtherArtist for split paying
 class Track(models.Model):
-    media = models.FileField()
+    name = models.CharField(max_length=150)
+    media = models.FileField(upload_to='media')
+    OtherArtists = models.ManyToManyField(OtherArtist,
+                                          related_name='otherArtists_set')
 
 
 # Album is Album(title=String, artist=Artist,
@@ -55,18 +86,16 @@ class Track(models.Model):
 #         artwork is the album cover image
 #         artist is the media's Artist
 #         genre, subgenre is the media's genre
-#         appleArtist, spotifyArtist is the URL
 #         partners is Partner
-#         otherArtists is OtherArtist for split paying
 class Album(models.Model):
 
-    class MediaType(models.IntegerChoices):
+    class mediaType(models.IntegerChoices):
         SINGLE = 0, _('Single')
         EP = 1, _('EP')
         ALBUM = 2, _('Album')
         __empty__ = _('Please Select...')
 
-    mediaType = models.IntegerField(choices=MediaType.choices)
+    mediaType = models.IntegerField(choices=mediaType.choices)
     tracks = models.ManyToManyField(Track,
                                     related_name='tracks_set')
     title = models.CharField(max_length=150)
@@ -74,14 +103,60 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     genre = models.CharField(max_length=50)
     subgenre = models.CharField(max_length=50, null=True)
-    appleArtist = models.URLField(null=True)
-    spotifyArtist = models.URLField(null=True)
     recordLabel = models.CharField(max_length=150)
     releaseDate = models.DateField()
-    partners = models.ManyToManyField(Partner,
-                                      related_name='partners_set')
-    OtherArtists = models.ManyToManyField(OtherArtist,
-                                          related_name='otherArtists_set')
+    platforms = models.ManyToManyField(Platform,
+                                       related_name='partners_set')
 
     def __str__(self):
         return self.title + ' by ' + self.artist.name
+
+
+'''
+-----------------------------
+    Content ID Model
+-----------------------------
+         ContentID
+-----------------------------
+'''
+
+
+# ContentID is ContentID(title=String, url=URL, status=statusType Choices)
+# interp. ContentID is Youtube removal request
+#         title is the Media's title
+#         url is media's URL
+#         status is statusType
+class ContentID(models.Model):
+
+    class statusType(models.IntegerChoices):
+        PENDING = 0, _('Pending')
+        RECIEVED = 1, _('Removal Request Received')
+        __empty__ = _('Please Select...')
+
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    status = models.IntegerField(choices=statusType.choices)
+
+
+'''
+-----------------------------
+     Statistic Models
+-----------------------------
+     Statistic, Statement
+-----------------------------
+'''
+
+
+# Statistic is Statistic(track=Track, Platform, stream=Integer,
+#                        download=Integer, revenue=Integer)
+# interp. Statistic is Track's statistic
+#         track is track with statistic
+#         platform is Platform
+#         stream is the stream count of media on that platform
+#         revenue is the money comes from platform
+class Statistic(models.Model):
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    stream = models.IntegerField()
+    download = models.IntegerField()
+    revenue = models.IntegerField()
