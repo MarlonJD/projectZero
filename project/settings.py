e8f1181a1bd4f98a8c2405e2910a83fe2b4f1e91
Burak Karahan <burak.karahan@mail.ru>
2020-01-23 19:10:20 +0300

    Change for production

--- project/settings.py
+++ project/settings.py
@@ -89,19 +89,8 @@ if os.environ['DEBUG']:
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
-            'HOST': os.environ['DB_HOST'],
-            'PORT': os.environ['DB_PORT'],
-        }
-    }
-
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600,
+                                                  ssl_require=True)
 
 
 # Password validation
