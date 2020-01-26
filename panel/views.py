6cb4e148355b5db88fac8f3b034017778fced070
Burak Karahan <burak.karahan@mail.ru>
2020-01-26 16:32:41 +0300

    Add split pay to backend

--- panel/views.py
+++ panel/views.py
@@ -3,9 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.urls import reverse_lazy
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
+from django.contrib.auth.models import User
 from django.views.generic import CreateView, ListView, TemplateView
 from .models import (Album, Artist, Track, Platform,
-                     ContentID, Statistic, Statement)
+                     ContentID, Statistic, Statement,
+                     OtherArtist)
 import json
 
 
@@ -77,7 +81,7 @@ def addDistUserView(request):
             for val in tracks:
                 trackObj = Track.objects.get(pk=val['id'])
                 albumObj.tracks.add(trackObj)
-                
+
             return JsonResponse({"success": 1}, status=200)
         except:
             JsonResponse({"Message": "Couldn't add distribution for some reason"}, status=418)
@@ -86,9 +90,29 @@ def addDistUserView(request):
             artistObj = Artist.objects.get(user=request.user)
         except Artist.DoesNotExist:
             artistObj = None
-        
-        return render(request, 'panel/addDist.html', {'platforms': Platform.objects.all,
-                                                      'artist': artistObj})
+
+        return render(request, 'panel/addDist.html',
+                      {'platforms': Platform.objects.all,
+                       'artist': artistObj})
+
+
+# Slug generator on model
+def username_generator(name, model, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(name)
+
+    qs_exists = model.objects.filter(username=slug).exists()
+
+    if qs_exists:
+        new_slug = "{slug}{randstr}".format(
+            slug=slug,
+            randstr=get_random_string(2)
+        )
+        return username_generator(name, model, new_slug=new_slug)
+
+    return slug
 
 
 @login_required
@@ -99,20 +123,41 @@ def trackUploadUser(request):
     try:
         number = request.POST['number']
         name = request.POST['name']
+        splitPays = json.loads(request.POST['splitPay'])
+
         t = Track.objects.create(number=number,
                                  name=name,
                                  media=request.FILES['media'])
-        return JsonResponse({"id": t.id,
-                             "number": '#' + str(t.number),
-                             "name": t.name,
-                             "fileName": t.media.name,
-                             "url": t.media.url,
-                             "size": str(round((t.media.size / 1048576), 2)) + ' MB'},
-                            status=200)
+
+        for s in splitPays:
+            try:
+                userObj = User.objects.get(email=s['email'])
+            except User.DoesNotExist:
+                userObj = User.objects.create_user(
+                    username_generator(s['email'].split('@')[0], User),
+                    s['email'],
+                    'temppass')
+
+            try:
+                tempArtist = Artist.objects.get(user=userObj)
+            except Artist.DoesNotExist:
+                tempArtist = Artist.objects.create(user=userObj,
+                                                   name=s['email'].split('@')[0])
+
+            otherArtistObj = OtherArtist.objects.create(user=userObj,
+                                                        artist=tempArtist,
+                                                        rate=s['rate'])
+            t.OtherArtists.add(otherArtistObj)
+
+            return JsonResponse({"id": t.id,
+                                 "number": '#' + str(t.number),
+                                 "name": t.name,
+                                 "fileName": t.media.name,
+                                 "url": t.media.url,
+                                 "size": str(round((t.media.size / 1048576), 2)) + ' MB'},
+                                status=200)
     except:
-        return JsonResponse({
-            "FailedCode": 0,
-            "Message": "Couldn't upload media for some reason"}, status=418)
+        JsonResponse({"Message": "Couldn't add split pay for some reason"}, status=418)
 
 
 '''
